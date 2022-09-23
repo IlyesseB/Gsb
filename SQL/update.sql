d66c32a9d9d8c6f79d5fe25acd271a49db4fa34e
IlyesseB <98451756+IlyesseB@users.noreply.github.com>
2022-09-23 09:16:50 +0200

    table

--- SQL/update.sql
+++ SQL/update.sql
@@ -1,4 +1,3 @@
-
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
@@ -34,6 +33,8 @@ CREATE TABLE `FicheFrais` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `FicheFrais` (`idVisiteur`, `mois`, `nbJustificatifs`, `montantValide`, `dateModif`, `idEtat`) VALUES
+('',	'02',	0,	0.00,	'2022-03-01',	'CL'),
+('',	'03',	0,	0.00,	'2022-03-01',	'CR'),
 ('a131',	'02',	0,	0.00,	'2022-02-09',	'CR'),
 ('a17',	'03',	0,	0.00,	'2022-03-02',	'CR'),
 ('e10',	'03',	0,	0.00,	'2022-03-02',	'CR');
@@ -65,6 +66,14 @@ CREATE TABLE `LigneFraisForfait` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `LigneFraisForfait` (`idVisiteur`, `mois`, `idFraisForfait`, `quantite`) VALUES
+('',	'02',	'ETP',	0),
+('',	'02',	'KM',	0),
+('',	'02',	'NUI',	0),
+('',	'02',	'REP',	0),
+('',	'03',	'ETP',	1),
+('',	'03',	'KM',	10),
+('',	'03',	'NUI',	100),
+('',	'03',	'REP',	1000),
 ('a131',	'02',	'ETP',	0),
 ('a131',	'02',	'KM',	0),
 ('a131',	'02',	'NUI',	0),
@@ -86,16 +95,22 @@ CREATE TABLE `LigneFraisHorsForfait` (
   `libelle` varchar(100) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `montant` decimal(10,2) DEFAULT NULL,
-  `modedepaiement` int(11) DEFAULT NULL,
+  `mdp` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `idVisiteur` (`idVisiteur`,`mois`),
   KEY `date` (`date`),
-  KEY `mdp` (`modedepaiement`),
+  KEY `mdp` (`mdp`),
   CONSTRAINT `LigneFraisHorsForfait_ibfk_1` FOREIGN KEY (`idVisiteur`, `mois`) REFERENCES `FicheFrais` (`idVisiteur`, `mois`),
-  CONSTRAINT `LigneFraisHorsForfait_ibfk_2` FOREIGN KEY (`modedepaiement`) REFERENCES `ModePaim` (`id`)
+  CONSTRAINT `LigneFraisHorsForfait_ibfk_2` FOREIGN KEY (`mdp`) REFERENCES `ModePaim` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `LigneFraisHorsForfait` (`id`, `idVisiteur`, `mois`, `libelle`, `date`, `montant`, `modedepaiement`) VALUES
+INSERT INTO `LigneFraisHorsForfait` (`id`, `idVisiteur`, `mois`, `libelle`, `date`, `montant`, `mdp`) VALUES
+(1,	'e10',	'03',	'randiom',	'2022-03-02',	552.00,	1),
+(2,	'',	'03',	'gfgf',	'2022-02-05',	5000.00,	NULL),
+(3,	'',	'03',	'velo',	'2022-03-05',	1250.00,	NULL),
+(4,	'',	'03',	'TEST111',	'2022-02-05',	55.00,	NULL),
+(5,	'',	'03',	'TEST111',	'2022-02-05',	55.00,	NULL),
+(6,	'',	'03',	'TEST111',	'2022-02-05',	55.00,	NULL);
 
 DROP TABLE IF EXISTS `ModePaim`;
 CREATE TABLE `ModePaim` (
@@ -109,15 +124,6 @@ INSERT INTO `ModePaim` (`id`, `lib`) VALUES
 (2,	'cheque'),
 (3,	'espece'),
 (4,	'crypto');
-
-DROP TABLE IF EXISTS `TypeEnergie`;
-CREATE TABLE `TypeEnergie` (
-  `libelleenergie` char(20) NOT NULL,
-  PRIMARY KEY (`libelleenergie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `TypeEnergie` (`libelleenergie`) VALUES
-('Thermique');
 
 DROP TABLE IF EXISTS `Visiteur`;
 CREATE TABLE `Visiteur` (
@@ -134,11 +140,11 @@ CREATE TABLE `Visiteur` (
   `numero` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `numero` (`numero`),
-  CONSTRAINT `Visiteur_ibfk_2` FOREIGN KEY (`numero`) REFERENCES `Voiture` (`numero`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `Visiteur_ibfk_1` FOREIGN KEY (`numero`) REFERENCES `Voiture` (`numero`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `Visiteur` (`id`, `nom`, `prenom`, `login`, `mdp`, `adresse`, `cp`, `ville`, `dateEmbauche`, `telephone`, `numero`) VALUES
-('b04',	'Bouraghda',	'Ilyesse',	'admin',	'password',	'osef',	'42170',	'st just',	NULL,	'06 51 45 65 78',	'178-86-58'),
+('b04',	'Bouraghda',	'Ilyesse',	'admin',	'password',	'osef',	'42170',	'st just',	NULL,	'06 51 45 65 78',	'FV-052-VF'),
 ('a131',	'Villechalane',	'Louis',	'lvillachane',	'jux7g',	'8 rue des Charmes',	'46000',	'Cahors',	'2005-12-21',	'01 01 01 10 02',	NULL),
 ('a17',	'Andre',	'David',	'dandre',	'oppg5',	'1 rue Petit',	'46200',	'Lalbenque',	'1998-11-23',	'01 01 01 10 05',	NULL),
 ('a55',	'Bedos',	'Christian',	'cbedos',	'gmhxd',	'1 rue Peranud',	'46250',	'Montcuq',	'1995-01-12',	'01 01 01 10 56',	NULL),
@@ -171,11 +177,133 @@ INSERT INTO `Visiteur` (`id`, `nom`, `prenom`, `login`, `mdp`, `adresse`, `cp`, 
 DROP TABLE IF EXISTS `Voiture`;
 CREATE TABLE `Voiture` (
   `numero` varchar(20) NOT NULL,
-  `type` char(20) DEFAULT NULL,
-  PRIMARY KEY (`numero`),
-  KEY `type` (`type`),
-  CONSTRAINT `Voiture_ibfk_2` FOREIGN KEY (`type`) REFERENCES `TypeEnergie` (`libelleenergie`) ON DELETE CASCADE ON UPDATE CASCADE
+  `constructeur` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  PRIMARY KEY (`numero`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `Voiture` (`numero`, `type`) VALUES
-('178-86-58',	'Thermique');
+('FV-052-VF',	'diesel');
+
+-- 2022-03-08 10:13:33
+
+
+f1fob
+f1fob
+
+
+-- Adminer 4.8.1 MySQL 5.5.5-10.5.12-MariaDB-0+deb11u1 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `APOURTAG`;
+CREATE TABLE `APOURTAG` (
+  `ref photo` int(10) NOT NULL,
+  `ref tag` varchar(100) NOT NULL,
+  PRIMARY KEY (`ref photo`,`ref tag`),
+  KEY `ref tag` (`ref tag`),
+  CONSTRAINT `APOURTAG_ibfk_1` FOREIGN KEY (`ref photo`) REFERENCES `PHOTO` (`id`),
+  CONSTRAINT `APOURTAG_ibfk_2` FOREIGN KEY (`ref tag`) REFERENCES `TAG` (`libelleTag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `NOTER`;
+CREATE TABLE `NOTER` (
+  `refUtilisateur` varchar(40) NOT NULL,
+  `refPhoto` int(40) NOT NULL,
+  `note` int(11) NOT NULL,
+  PRIMARY KEY (`refPhoto`,`refUtilisateur`),
+  KEY `ref Utilisateur` (`refUtilisateur`),
+  KEY `note` (`note`),
+  CONSTRAINT `NOTER_ibfk_1` FOREIGN KEY (`refUtilisateur`) REFERENCES `UTILISATEUR` (`mail Utilisateur`),
+  CONSTRAINT `NOTER_ibfk_2` FOREIGN KEY (`refPhoto`) REFERENCES `PHOTO` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `NOTER` (`refUtilisateur`, `refPhoto`, `note`) VALUES
+('ilyesse.bouraghda@gmail.com',	1,	4),
+('idriss.hml@gmail.com',	3,	5),
+('idriss.hml@gmail.com',	1,	6),
+('Antoine.bizo@gmail.com',	2,	6),
+('Antoine.bizo@gmail.com',	1,	8),
+('ilyesse.bouraghda@gmail.com',	2,	8),
+('ilyesse.bouraghda@gmail.com',	3,	8),
+('idriss.hml@gmail.com',	2,	9),
+('Antoine.bizo@gmail.com',	3,	10);
+
+DROP TABLE IF EXISTS `PHOTO`;
+CREATE TABLE `PHOTO` (
+  `id` int(10) NOT NULL,
+  `lien` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `ref photographe` varchar(40) NOT NULL,
+  `ref appareil` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ref photographe` (`ref photographe`),
+  KEY `ref appareil` (`ref appareil`),
+  CONSTRAINT `PHOTO_ibfk_1` FOREIGN KEY (`ref photographe`) REFERENCES `PHOTOGRAPHE` (`mail`),
+  CONSTRAINT `PHOTO_ibfk_2` FOREIGN KEY (`ref appareil`) REFERENCES `TYPE APPAREIL` (`le type Appareil`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `PHOTO` (`id`, `lien`, `date`, `ref photographe`, `ref appareil`) VALUES
+(1,	'www.photo1.fr',	'2022-03-30',	'cristiano.ronaldo@gmail.com',	'appareils photo bridge'),
+(2,	'www.photo2.fr',	'2022-03-30',	'stephane@gmail.com',	'appareils photo compact'),
+(3,	'www.photo3.fr',	'2022-03-30',	'steve.jobs@gmail.com',	'appareils photo reflex');
+
+DROP TABLE IF EXISTS `PHOTOGRAPHE`;
+CREATE TABLE `PHOTOGRAPHE` (
+  `mail` varchar(40) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `mot de passe` varchar(50) NOT NULL,
+  PRIMARY KEY (`mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `PHOTOGRAPHE` (`mail`, `nom`, `prenom`, `mot de passe`) VALUES
+('cristiano.ronaldo@gmail.com',	'Ronaldo dos Santos Aveiro',	'cristiano',	'cr7'),
+('stephane@gmail.com',	'stephane',	'islo',	'stephane10'),
+('steve.jobs@gmail.com',	'steve',	'jobs',	'jobste');
+
+DROP TABLE IF EXISTS `TAG`;
+CREATE TABLE `TAG` (
+  `libelleTag` varchar(100) NOT NULL,
+  PRIMARY KEY (`libelleTag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `TARIF`;
+CREATE TABLE `TARIF` (
+  `note moyenne` int(11) NOT NULL,
+  `prix` int(11) NOT NULL,
+  PRIMARY KEY (`note moyenne`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `TYPE APPAREIL`;
+CREATE TABLE `TYPE APPAREIL` (
+  `le type Appareil` varchar(40) NOT NULL,
+  PRIMARY KEY (`le type Appareil`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `TYPE APPAREIL` (`le type Appareil`) VALUES
+('appareils photo bridge'),
+('appareils photo compact'),
+('appareils photo reflex');
+
+DROP TABLE IF EXISTS `UTILISATEUR`;
+CREATE TABLE `UTILISATEUR` (
+  `mail Utilisateur` varchar(40) NOT NULL,
+  `mot de passe` varchar(50) NOT NULL,
+  PRIMARY KEY (`mail Utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `UTILISATEUR` (`mail Utilisateur`, `mot de passe`) VALUES
+('Antoine.bizo@gmail.com',	'Antoine453'),
+('idriss.hml@gmail.com',	'Idriss123'),
+('ilyesse.bouraghda@gmail.com',	'Ilyesse000');
+
+-- 2022-03-30 07:59:00
